e51df77a8dfdebc6eb3bef00010347aeafe282bb
Irinalucky <irinalaporte@gmail.com>
2021-01-11 12:14:54 +0100

    add header

--- inc/header.php
+++ inc/header.php
@@ -7,4 +7,10 @@
     <link rel="stylesheet" href="/assets/css/style.css">
 </head>
 <body>
-    
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Accueil</a></li>
+            </ul>
+        </nav>
+    </header>
