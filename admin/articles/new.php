9e1d2b888436f343ac86346ab0a63d14a5755039
Neutron-Pro <neutron@neutronstars.fr>
2021-01-11 16:55:06 +0100

    Add create article page

--- admin/articles/new.php
+++ admin/articles/new.php
@@ -1,11 +1,48 @@
 <?php
 
-    require '../../inc/database.php';
     require '../inc/functions.php';
 
     $urlBase = '../';
     include '../inc/header.php';
 
+    $errors = [];
 
+    if(!empty($_POST['submitted']))
+    {
+        $title = secureTextByArray($_POST, 'title');
+        $author = secureTextByArray($_POST, 'author');
+        $description = secureTextByArray($_POST, 'description');
+        $content = secureTextByArray($_POST, 'content');
+
+        checkLengthTextValid($title, 5, 255, $errors, 'title');
+        checkLengthTextValid($author, 4, 50, $errors, 'author');
+        checkLengthTextValid($description, 10, 255, $errors, 'description');
+        checkLengthTextValid($content, 10, 99_999_999_999, $errors, 'content');
+
+        if(empty($errors))
+        {
+            require '../../inc/database.php';
+            newArticle($pdo, $author, $title, $description, $content);
+            header('Location: ../');
+            exit;
+        }
+    }
+
+?>
+    <div class="container">
+        <section id="newArticle">
+            <h2>Ajouter un nouvel article</h2>
+            <form action="" method="post">
+                <?php
+                    buildInputByArray($_POST, 'Titre *', 'text', 'title', $errors);
+                    buildInputByArray($_POST, 'Auteur *', 'text', 'author', $errors);
+                    buildInputByArray($_POST, 'Description *', 'text', 'description', $errors);
+                    buildTextArea($_POST, 'Contenu *', 'content', 10, $errors);
+                ?>
+                <input type="submit" name="submitted" value="Ajouter">
+            </form>
+        </section>
+    </div>
+<?php
 
     include '../inc/footer.php';
--- admin/inc/functions.php
+++ admin/inc/functions.php
@@ -13,3 +13,135 @@ function getArticles(PDO $pdo)
     $query->execute();
     return $query->fetchAll();
 }
+
+function newArticle(PDO $pdo, string $author, string $title, string $description, string $content)
+{
+    $query = $pdo -> prepare('INSERT INTO articles (title,author,description,content,created_at) VALUES (:title, :author, :description, :content, NOW())');
+    $query->bindValue(':title', $title);
+    $query->bindValue(':author', $author);
+    $query->bindValue(':description', $description);
+    $query->bindValue(':content', $content);
+    $query->execute();
+}
+
+function getValueByArray(array $array, string $key, string $defaultValue = '') {
+    return !empty($array[$key]) ? $array[$key] : $defaultValue;
+}
+
+function secureTextByArray(array $array, string $key): string
+{
+    return secureText(getValueByArray($array, $key));
+}
+
+function secureText(string $text): string
+{
+    return trim(strip_tags($text));
+}
+
+function checkLengthTextValid(string $text, int $min, int $max, array &$errors, string $key): void
+{
+    if( empty($text) ) {
+        $errors[$key] = 'Veuillez renseigner ce champs !';
+    } elseif (mb_strlen($text) < $min ) {
+        $errors[$key] = 'Ce champs ne contient pas assez de caractère !';
+    } elseif (mb_strlen($text) > $max) {
+        $errors[$key] = 'Ce champs contient trop de caractère !';
+    }
+}
+
+function checkNumericValid($number, array &$errors, string $key, int $min = PHP_INT_MIN, int $max = PHP_INT_MAX)
+{
+    if( empty($number) ) {
+        $errors[$key] = 'Veuillez renseigner ce champs !';
+    }elseif ( !filter_var($number, FILTER_VALIDATE_INT) ) {
+        $errors[$key] = 'La valeur saisie n\'est pas un nombre valide !';
+    }elseif (intval($number) < $min) {
+        $errors[$key] = 'La valeur saisie est trop petite !';
+    }elseif (intval($number) > $max) {
+        $errors[$key] = 'La valeur saisie est trop grande !';
+    }
+}
+
+function checkEmailValid(string $text, array &$errors, string $key): void
+{
+    if(empty($text)){
+        $errors[$key] = 'Veuillez renseigner ce champs !';
+    } elseif(!filter_var($text, FILTER_VALIDATE_EMAIL)) {
+        $errors[$key] = 'Ceci n\'est pas un email valide !';
+    }
+}
+
+function checkValueSelect(array $array, string $value, array &$errors, string $key): void
+{
+    if(empty($value) || empty($array[$value])) {
+        $errors[$key] = 'Veuillez renseigner ce champs !';
+    }
+}
+
+function buildInputByArray(array $array, string $label, string $type, string $id, array $errors) {
+    buildInput((!empty($array[$id]) ? $array[$id] : ''), $label, $type, $id, $errors);
+}
+
+function buildInput(string $text, string $label, string $type, string $id, array $errors){?>
+    <div class="form-group">
+        <label for="<?=$id?>"><?=$label?></label>
+        <input type="<?=$type?>" name="<?=$id?>" id="<?=$id?>"  value="<?=$text?>"<?= !empty($errors[$id]) ? ' class="error"' : '' ?>>
+        <span class="error"><?= !empty($errors[$id]) ? $errors[$id] : '' ?></span>
+    </div>
+<?php }
+
+function buildTextArea(array $array, string $label, string $id, int $rows, array $errors){ ?>
+    <div class="form-group">
+        <label for="<?=$id?>"><?=$label?></label>
+        <textarea name="<?=$id?>" id="<?=$id?>" rows="<?=$rows?>"<?= !empty($errors[$id]) ? ' class="error"' : '' ?>><?= !empty($array[$id]) ? $array[$id] : '' ?></textarea>
+        <span class="error"><?= !empty($errors[$id]) ? $errors[$id] : '' ?></span>
+    </div>
+<?php }
+
+function buildSelect(string $label, string $id, array $values, $defaultValue, array $errors) { ?>
+    <div class="form-group">
+        <label for="<?=$id?>"><?=$label?></label>
+        <select name="<?=$id?>" id="<?=$id?>"<?= !empty($errors[$id]) ? ' class="error"' : '' ?>>
+            <?php foreach ($values as $key => $value): ?>
+                <option value="<?=$key?>"<?= $key == $defaultValue ? ' selected' : '' ?>><?=$value?></option>
+            <?php endforeach; ?>
+        </select>
+        <span class="error"><?= !empty($errors[$id]) ? $errors[$id] : '' ?></span>
+    </div>
+<?php }
+
+function buildPagination($count, $currentPage, $limit, $range = 3, $url='/?') {
+    if($count <= $limit) { return; }
+    $maxPage = ceil($count/$limit); ?>
+    <div class="pagination">
+        <?php
+        for ($i = 1; ($maxPage > ($range*2) && $i <= $range) || ($maxPage <= ($range*2) && $i <= $maxPage); $i++){
+            buildButtonPage($url.'page='.$i, $i, $i == $currentPage);
+        }
+        if($maxPage > $range) {
+            if($currentPage >= ($range*2)) { ?>
+                <span class="pagination-split">...</span>
+            <?php }
+            for($i = $maxPage-($range-1); $i < $currentPage-(($range-1)/2); $i++){
+                buildButtonPage($url.'page='.$i, $i, false);
+            }
+            for($i = $currentPage - (($range-1)/2); $i <= $currentPage+(($range-1)/2); $i++) {
+                if($i > $range && $i <= $maxPage) {
+                    buildButtonPage($url.'page='.$i, $i, $i == $currentPage);
+                }
+            }
+            if($maxPage-($range-1) > $currentPage+((($range-1)/2)+1)) { ?>
+                <span class="pagination-split">...</span>
+            <?php }
+            for($i = $maxPage-($range-1); $i <= $maxPage; $i++) {
+                if($i > $currentPage+(($range-1)/2)) {
+                    buildButtonPage($url.'page='.$i, $i, $i == $currentPage);
+                }
+            }
+        } ?>
+    </div>
+<?php }
+
+function buildButtonPage($url, $index, $selected) { ?>
+    <a href="<?=$url?>"<?=$selected ? 'class="active"' : ''?>><?=$index?></a>
+<?php }
