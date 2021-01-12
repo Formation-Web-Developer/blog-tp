1c4915111083a9253633d3fefbf57d0c3d17640c
Neutron-Pro <neutron@neutronstars.fr>
2021-01-12 16:45:28 +0100

    add name to header

--- admin/index.php
+++ admin/index.php
@@ -30,33 +30,35 @@ include('inc/header.php');
             <div class="badge badge-success">
                 L'article a bien été supprimé !
             </div>
+        <?php }
+
+        if(hasRole($_SESSION, ADMINISTRATOR)) { ?>
+            <section id="articles">
+                <h2>Liste des articles</h2>
+                <a href="articles/new.php" class="btn btn-success add">Ajouter un article</a>
+                <div class="articles">
+                    <?php foreach ($articles as $article) :
+                        $published = $article['visibility'] == 1; ?>
+                        <div class="article">
+                            <div class="article-header">
+                                <h3><?=$article['title']?></h3>
+                                <p class="description"><?=$article['description']?></p>
+                                <p class="misc">De <span class="misc-author"><?=$article['author']?></span> le <?=date("d/M/Y à H:i", strtotime($published ? $article['published_at'] : $article['created_at']))?></p>
+                            </div>
+
+                            <div class="article-footer">
+                                <p class="misc-state"><i class="fas fa-circle <?=$published ? 'publish' : 'draft'?>"></i> <?=$published ? 'Publié' : 'Brouillon'?></p>
+                                <a href="articles/edit.php?id=<?=$article['id']?>" class="btn btn-primary">Modifier</a>
+                                <form action="" method="post" onsubmit="return confirmDeleteArticle()">
+                                    <input type="hidden" name="id" value="<?=$article['id']?>">
+                                    <input type="submit" class="btn btn-danger" value="Supprimer" name="deleted" />
+                                </form>
+                            </div>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            </section>
         <?php } ?>
-
-        <section id="articles">
-            <h2>Liste des articles</h2>
-            <a href="articles/new.php" class="btn btn-success add">Ajouter un article</a>
-            <div class="articles">
-                <?php foreach ($articles as $article) :
-                    $published = $article['visibility'] == 1; ?>
-                    <div class="article">
-                        <div class="article-header">
-                            <h3><?=$article['title']?></h3>
-                            <p class="description"><?=$article['description']?></p>
-                            <p class="misc">De <span class="misc-author"><?=$article['author']?></span> le <?=date("d/M/Y à H:i", strtotime($published ? $article['published_at'] : $article['created_at']))?></p>
-                        </div>
-
-                        <div class="article-footer">
-                            <p class="misc-state"><i class="fas fa-circle <?=$published ? 'publish' : 'draft'?>"></i> <?=$published ? 'Publié' : 'Brouillon'?></p>
-                            <a href="articles/edit.php?id=<?=$article['id']?>" class="btn btn-primary">Modifier</a>
-                            <form action="" method="post" onsubmit="return confirmDeleteArticle()">
-                                <input type="hidden" name="id" value="<?=$article['id']?>">
-                                <input type="submit" class="btn btn-danger" value="Supprimer" name="deleted" />
-                            </form>
-                        </div>
-                    </div>
-                <?php endforeach; ?>
-            </div>
-        </section>
     </div>
 <?php
 
